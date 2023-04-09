3bd9570310520233f0425261f5a7a60f93935159
karoldawidg <karoldawidg@gmail.com>
2023-04-10 01:07:24 +0200

    Log 21: new css file

--- backend/database/TaskRecord.js
+++ backend/database/TaskRecord.js
@@ -2,7 +2,7 @@ const {pool} = require("./db");
 
 class TaskRecord{
     static async listAll(nameUser){
-        const [results] = await pool.execute('SELECT * FROM tasks WHERE user = ?',[nameUser]);
+        const [results] = await pool.execute('SELECT * FROM tasks WHERE user = ? ORDER BY nr DESC' ,[nameUser]);
         return results;
     }
 }
