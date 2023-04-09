e6a9fac046662c2b8119d1250c653bb9ca1d2a00
karoldawidg <karoldawidg@gmail.com>
2023-04-10 01:49:14 +0200

    Log 23: add new fetch in emailSender.js

--- public/js/emailSender.js
+++ public/js/emailSender.js
@@ -1,32 +1,34 @@
 const contactForm = document.querySelector('.contact-form');
-//pobiera elementy poprzez ID
-let namee = document.getElementById('name');        //dane sa pobierane przez ID
-let email = document.getElementById('email');
-let subject = document.getElementById('subject');
-let message = document.getElementById('message');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const subjectInput = document.querySelector('#subject');
+const messageInput = document.querySelector('#message');
 
-contactForm.addEventListener("submit", (e)=>{
+contactForm.addEventListener('submit', async (e) => {
     e.preventDefault();
-    let formData ={                                 //dane sa przekazywane jako obiekt do zmiennej formData
-        name: namee.value,
-        email: email.value,
-        subject: subject.value,
-        message: message.value
+
+    const formData = {
+        name: nameInput.value,
+        email: emailInput.value,
+        subject: subjectInput.value,
+        message: messageInput.value,
+    };
+
+    const response = await fetch('/form', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+    });
+
+    if (response.ok && (await response.text()).includes('success')) {
+        alert('An E-mail has been sent');
+        nameInput.value = '';
+        emailInput.value = '';
+        subjectInput.value = '';
+        messageInput.value = '';
+    } else {
+        alert('Something went wrong!');
     }
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', '/form');
-    xhr.setRequestHeader('content-type', 'application/json');
-    xhr.onload =  function(){
-        console.log(xhr.responseText);
-            if(xhr.responseText === 'success'){
-                alert('An E-mail has been sent');
-                namee.value = '',
-                email.value = '',
-                subject.value = '',
-                message.value = ''
-            }else{
-                alert('Somethng is no yes!');
-            }
-    }
-    xhr.send(JSON.stringify(formData));
 });
