db492abb33fdf1a8cc7faf0d6dfdb518c8d8a966
karoldawidg <karoldawidg@gmail.com>
2023-04-14 07:06:24 +0200

    Log 29: Removal of express-session components

--- backend/config/configENV.js
+++ backend/config/configENV.js
@@ -3,12 +3,11 @@ require('dotenv').config();
 module.exports = {
     pass: process.env.PASS,
     user: process.env.USER,
-    hostDB:process.env.HOST_DB, 
-    nameDB:process.env.NAME_DB, 
-    userDB:process.env.USER_DB, 
-    passDB:process.env.PASS_DB,
-    PORT:process.env.PORT,
-    SECRET:process.env.SECRET,
+    hostDB:process.env.HOST_DB || 'localhost', 
+    nameDB:process.env.NAME_DB || 'nodelogin', 
+    userDB:process.env.USER_DB || 'root', 
+    passDB:process.env.PASS_DB || '',
+    PORT:process.env.PORT || 3000,
     JWT_SECRET:process.env.JWT_SECRET,
     service:process.env.service,
 };
